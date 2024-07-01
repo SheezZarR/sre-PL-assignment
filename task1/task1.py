5c712efdeec924453f28c1d8b11677d85e7fde8a
Iaroslav Baslin <wertout77@gmail.com>
2024-07-01 19:38:23 +0300

    draft: task1 completed

---
+++ task1/task1.py
@@ -0,0 +1,47 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Enter n, m')
+parser.add_argument('n', type=int)
+parser.add_argument('m', type=int)
+args = parser.parse_args()
+
+def n_more_than_m(left: int, right: int, arr: list) -> None:
+    slice = arr[left : right]
+    ans = ""
+
+    while slice[len(slice) - 1] != arr[0]:
+        ans += str(slice[0])
+
+        left = (left + args.m - 1) % args.n
+        right = (right + args.m - 1) % args.n
+        
+        if left > right:
+            slice = arr[left : args.n]
+            slice += arr[:right]
+
+        else:
+            slice = arr[left : right]
+
+    
+    ans += str(slice[0])
+    print(ans)
+
+def n_leq_m(left: int, right: int, arr: list) -> None:
+    pass
+
+def main():
+    # objective print arrays until first element is the last
+    arr = [(i+1) for i in range(args.n)]
+    left = 0
+    right = args.m
+
+    if right < args.n:
+        n_more_than_m(left, right, arr)
+        
+    else:
+        # absolutely clueless
+        n_leq_m(left, right, arr)
+
+
+if __name__ == "__main__":
+    main()
