922e3f00058b73846317c592d86d571e729100a5
Iaroslav Baslin <wertout77@gmail.com>
2024-07-03 13:53:47 +0300

    fix: task1 complete solution

--- task1/task1.py
+++ task1/task1.py
@@ -5,7 +5,8 @@ parser.add_argument('n', type=int)
 parser.add_argument('m', type=int)
 args = parser.parse_args()
 
-def n_more_than_m(left: int, right: int, arr: list) -> None:
+def generate_answer(left: int, right: int, arr: list) -> None:
+    # danger case: left = n, right = 2n
     slice = arr[left : right]
     ans = ""
 
@@ -15,32 +16,29 @@ def n_more_than_m(left: int, right: int, arr: list) -> None:
         left = (left + args.m - 1) % args.n
         right = (right + args.m - 1) % args.n
         
-        if left > right:
+        if left >= right:
             slice = arr[left : args.n]
             slice += arr[:right]
 
         else:
             slice = arr[left : right]
-
     
     ans += str(slice[0])
     print(ans)
 
-def n_leq_m(left: int, right: int, arr: list) -> None:
-    pass
 
 def main():
     # objective print arrays until first element is the last
+    # n >= 1
+    # m ? 
     arr = [(i+1) for i in range(args.n)]
     left = 0
     right = args.m
 
-    if right < args.n:
-        n_more_than_m(left, right, arr)
-        
-    else:
-        # absolutely clueless
-        n_leq_m(left, right, arr)
+    while (right > args.n):
+        right -= args.n
+
+    generate_answer(left, right, arr)
 
 
 if __name__ == "__main__":
